2772f786d0e1c27cb0eadbb7b79e1d4f9b3b5314
nameisvick <32327726+nameisvick@users.noreply.github.com>
2025-02-08 20:11:20 +0530

    Add files via upload

---
+++ LibraryManagement.java
@@ -0,0 +1,14 @@
+package week2.day1;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		Library checkresult = new Library();
+	checkresult.issueBook();
+	System.out.println(checkresult.addBook("Book added successfully"));
+		
+		
+
+	}
+
+}
